4b53a59b2b6b6cd252da3230c5648782134cd33d
Matt <mattstark256@gmail.com>
2019-06-08 00:29:06 +0100

    added even more levels

--- Twisty Jigsaw/Assets/Editor/SpiderRotatingPieceEditor.cs
+++ Twisty Jigsaw/Assets/Editor/SpiderRotatingPieceEditor.cs
@@ -8,6 +8,24 @@ public class SpiderRotatingPieceEditor : RotatingPieceEditor
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
+
+        SpiderRotatingPiece spiderPiece = (SpiderRotatingPiece)target;
+
+        EditorGUI.BeginChangeCheck();
+        GUILayout.Button("Rotate spider");
+        if (EditorGUI.EndChangeCheck())
+        {
+            foreach(Piece piece in spiderPiece.GetConnectedPieces())
+            {
+                if (piece == null) continue;
+
+                Undo.RecordObject(piece, "Rotate piece");
+                EditorUtility.SetDirty(piece);
+                piece.RotateTilesCW();
+                Vector3 relativePosition = piece.transform.localPosition - spiderPiece.transform.localPosition;
+                piece.transform.localPosition = spiderPiece.transform.localPosition + new Vector3(relativePosition.y, -relativePosition.x, relativePosition.z);
+            }
+        }
     }
 
 
