d8704d70533dda224fd79589e71c62cae3ec88f6
Matt <mattstark256@gmail.com>
2019-06-13 21:28:08 +0100

    improved some levels

---
+++ Twisty Jigsaw/Assets/FadeOut.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class FadeOut : MonoBehaviour
+{
+    [SerializeField]
+    private float fadeDuration = 1;
+
+
+    private void Start()
+    {
+        StartCoroutine(FadeOutCoroutine());
+    }
+    
+
+    private IEnumerator FadeOutCoroutine()
+    {
+        Image image = GetComponent<Image>();
+        Color initialColor = image.color;
+
+        float f = 0;
+        while (f<1)
+        {
+            f += Time.deltaTime / fadeDuration;
+            if (f > 1) f = 1;
+
+            image.color = new Color(initialColor.r, initialColor.g, initialColor.b, Mathf.SmoothStep(initialColor.a, 0, f));
+
+            yield return null;
+        }
+
+        Destroy(gameObject);
+    }
+}
